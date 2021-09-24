ac80cb63f05e49ccac88a105c926dd022efcfa0d
N-vdL <70437972+N-vdL@users.noreply.github.com>
2021-09-24 16:13:01 +0200

    Add files via upload

---
+++ AccountSystemWebSpringBoot/src/main/java/controller/RestServiceApplication.java
@@ -0,0 +1,22 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class RestServiceApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+}
